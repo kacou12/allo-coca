57004643a5d07ec1a3238a636850dd1359ff0ce3
Kacou Yannick <kacou@sendchap.ci>
2025-11-25 17:38:48 +0000

    feat: add banner out of stock

--- src/components/allococa/productsTabs/productFullLockerCard.vue
+++ src/components/allococa/productsTabs/productFullLockerCard.vue
@@ -1,49 +1,73 @@
 <template>
-    <div class="bg-white rounded-sm border p-1 relative h-[236px]">
-
-        <div class="absolute bottom-4 right-2 left-2 flex items-center justify-between">
-
-            <!-- <CommonButton class="h-[35px]" title="Approvisionner"
+  <div class="bg-white rounded-sm border p-1 relative h-[236px] overflow-hidden">
+    <div class="absolute bottom-4 right-2 left-2 flex items-center justify-between">
+      <!-- <CommonButton class="h-[35px]" title="Approvisionner"
                 @click="emit('fullIncrement', { ...product, quantity: 1 })"
                 :disabled="casierProductsData.products.length === 24 && casierProductsData.products.length > 0 && casierProductsData.products[0].id == product.id">
 
             </CommonButton> -->
 
-            <button :disabled="isDisabled" @click="emit('fullIncrement', { ...product, quantity: 1 })"
-                :class="[isDisabled ? 'bg-gray-300 cursor-not-allowed' : 'bg-primary-50 hover:bg-primary-40 text-white']"
-                class="w-full py-1  text-xs md:text-sm rounded-[90px] transition-colors duration-200 ">
-                Approvisionner
-            </button>
-        </div>
-
-        <!-- Nom du produit -->
-        <h3 class=" font-bold text-xs md:text-sm text-center mb-4 line-clamp-2">{{ product.product.name }} {{
-            toLowercaseFirstLetterOfEachWord(product.label)
-        }}</h3>
-
-        <!-- Image du produit -->
-        <div class=" ml-5">
-            <img :src="product.image_url" :alt="`Image de ${product.product.name}`"
-                class="object-cover w-[60px] max-h-[186px]" />
-        </div>
-
-        <!-- Informations prix et variante -->
-        <div class="space-y-0 absolute  top-1/2 text-xs right-[10px]">
-            <div class="flex justify-end">
-                <p class=" text-gray-500 font-medium">{{ product.size }}</p>
-            </div>
-            <div class="text-left">
-                <p class=" font-bold text-gray-800">{{ formatPrice(product.unit_price) }} </p>
-            </div>
-        </div>
+      <button
+        :disabled="isDisabled"
+        @click="emit('fullIncrement', { ...product, quantity: 1 })"
+        :class="[
+          isDisabled
+            ? 'bg-gray-300 cursor-not-allowed'
+            : 'bg-primary-50 hover:bg-primary-40 text-white',
+        ]"
+        class="w-full py-1 text-xs md:text-sm rounded-[90px] transition-colors duration-200"
+      >
+        Approvisionner
+      </button>
     </div>
+
+    <!-- Nom du produit -->
+    <h3 class="font-bold text-xs md:text-sm text-center mb-4 line-clamp-2">
+      {{ product.product.name }} {{ toLowercaseFirstLetterOfEachWord(product.label) }}
+    </h3>
+
+    <!-- Image du produit -->
+    <div class="ml-5">
+      <img
+        :src="product.image_url"
+        :alt="`Image de ${product.product.name}`"
+        class="object-cover w-[60px] max-h-[186px]"
+      />
+    </div>
+
+    <!-- Informations prix et variante -->
+    <div class="space-y-0 absolute top-1/2 text-xs right-[10px]">
+      <div class="flex justify-end">
+        <p class="text-gray-500 font-medium">{{ product.size }}</p>
+      </div>
+      <div class="text-left">
+        <p class="font-bold text-gray-800">{{ formatPrice(product.unit_price) }}</p>
+      </div>
+    </div>
+
+    <!-- out of stock banner -->
+    <section v-if="!product.status" class="pointer-events-noner absolute inset-0">
+      <!-- Fond semi-transparent -->
+      <div class="absolute inset-0 bg-gray-400/50"></div>
+
+      <!-- Bandeau diagonal -->
+      <div
+        class="absolute top-10 -right-24 w-80 bg-red-600 text-white text-center py-3 font-bold text-xs tracking-wider transform rotate-45 shadow-lg"
+      >
+        RUPTURE DE STOCK
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 // import Button from '@/components/ui/button/Button.vue';
-import type { CasierProduct, ProductResponse } from '@/services/locker-products/locker-products-type';
-import { formatPrice, toLowercaseFirstLetterOfEachWord } from '@/shared/shared';
-import { computed, type PropType } from 'vue';
+import type {
+  CasierProduct,
+  ProductResponse,
+} from '@/services/locker-products/locker-products-type'
+import { formatPrice, toLowercaseFirstLetterOfEachWord } from '@/shared/shared'
+import { computed, type PropType } from 'vue'
 
 // Définition des props
 // interface Props {
@@ -52,26 +76,29 @@ import { computed, type PropType } from 'vue';
 // }
 
 const { product, casierProductsData } = defineProps({
-    product: {
-        type: Object as PropType<ProductResponse>,
-        required: true
-    },
-    casierProductsData: {
-        type: Object as PropType<CasierProduct>,
-        required: true
-    }
-});
+  product: {
+    type: Object as PropType<ProductResponse>,
+    required: true,
+  },
+  casierProductsData: {
+    type: Object as PropType<CasierProduct>,
+    required: true,
+  },
+})
 
 // const imageUrl = computed(() => new URL(`../../../assets/allococa/products/${product.image}`, import.meta.url).href);
 
-const isDisabled = computed(() => casierProductsData.products.length === 24 && casierProductsData.products.length > 0 && casierProductsData.products[0].id == product.id);
+const isDisabled = computed(
+  () =>
+    casierProductsData.products.length === 24 &&
+    casierProductsData.products.length > 0 &&
+    casierProductsData.products[0].id == product.id,
+)
 
 // Déclaration de l'émetteur
 const emit = defineEmits<{
-    (e: 'fullIncrement', product: ProductResponse): void;
-}>();
-
-
+  (e: 'fullIncrement', product: ProductResponse): void
+}>()
 </script>
 
 <style scoped>
--- src/components/allococa/productsTabs/productLockerCard.vue
+++ src/components/allococa/productsTabs/productLockerCard.vue
@@ -1,59 +1,93 @@
 <template>
-    <div class="bg-white rounded-sm border p-1 relative h-[236px]">
-        <!-- Contrôles de quantité en position absolue -->
-        <section
-            class="absolute bottom-4 right-2 left-2 flex items-center justify-between rounded-full  px-2 py-1 border-[#E7E7E7] border-[1px] bg-[#F6F6F6]">
-            <button @click="decrement" :disabled="product.quantity === 0" :class="[
-                'rounded-full w-6 h-6 flex items-center justify-center transition-all duration-300',
-                product.quantity === 0
-                    ? 'bg-gray-100 text-gray-300 cursor-not-allowed'
-                    : 'border-[1px] border-[#3D3D3D] hover:bg-gray-200'
-                // : 'bg-gray-200 text-gray-600 hover:bg-gray-300 hover:text-gray-700'
-            ]">
-                <Minus :size="15"></Minus>
-            </button>
+  <div class="bg-white rounded-sm border p-1 h-[236px] relative overflow-hidden">
+    <!-- Contrôles de quantité en position absolue -->
+    <section
+      class="absolute bottom-4 right-2 left-2 flex items-center justify-between rounded-full px-2 py-1 border-[#E7E7E7] border-[1px] bg-[#F6F6F6]"
+    >
+      <button
+        @click="decrement"
+        :disabled="product.quantity === 0"
+        :class="[
+          'rounded-full w-6 h-6 flex items-center justify-center transition-all duration-300',
+          product.quantity === 0
+            ? 'bg-gray-100 text-gray-300 cursor-not-allowed'
+            : 'border-[1px] border-[#3D3D3D] hover:bg-gray-200',
+          // : 'bg-gray-200 text-gray-600 hover:bg-gray-300 hover:text-gray-700'
+        ]"
+      >
+        <Minus :size="15"></Minus>
+      </button>
 
-            <span class="text-sm md:text-lg font-semibold w-6 text-center text-gray-800">{{ product.quantity }}</span>
+      <span class="text-sm md:text-lg font-semibold w-6 text-center text-gray-800">{{
+        product.quantity
+      }}</span>
 
-            <button @click="increment" :disabled="product.quantity == 24 || casierProductsData.products.length == 24"
-                :class="[product.quantity == 24 || casierProductsData.products.length == 24 ? 'bg-gray-100 text-gray-300 cursor-not-allowed' : 'bg-red-600 hover:bg-red-700 text-white']"
-                class=" rounded-full  flex items-center justify-center transition-all duration-300 ">
-                <div class=" w-6 h-6 rounded-full flex items-center justify-center">
-                    <div class="">
-                        <Plus :size="15"></Plus>
-                    </div>
-                </div>
-            </button>
-        </section>
-
-        <!-- Nom du produit -->
-        <h3 class=" font-bold text-xs md:text-sm text-center mb-4 line-clamp-2">{{ product.product.name }} {{
-            toLowercaseFirstLetterOfEachWord(product.label)
-        }}</h3>
-
-        <!-- Image du produit -->
-        <div class=" ml-5">
-            <img :src="product.image_url" :alt="`Image de ${product.product.name}`"
-                class="object-cover w-[60px] max-h-[186px]" />
+      <button
+        @click="increment"
+        :disabled="product.quantity == 24 || casierProductsData.products.length == 24"
+        :class="[
+          product.quantity == 24 || casierProductsData.products.length == 24
+            ? 'bg-gray-100 text-gray-300 cursor-not-allowed'
+            : 'bg-red-600 hover:bg-red-700 text-white',
+        ]"
+        class="rounded-full flex items-center justify-center transition-all duration-300"
+      >
+        <div class="w-6 h-6 rounded-full flex items-center justify-center">
+          <div class="">
+            <Plus :size="15"></Plus>
+          </div>
         </div>
+      </button>
+    </section>
 
-        <!-- Informations prix et variante -->
-        <div class="space-y-0 absolute right-[10px] top-1/2 text-xs ">
-            <div class="flex justify-end">
-                <p class=" text-gray-500 font-medium">{{ product.size }}</p>
-            </div>
-            <div class="text-left">
-                <p class=" font-bold text-gray-800">{{ formatPrice(product.unit_price) }}</p>
-            </div>
-        </div>
+    <!-- Nom du produit -->
+    <h3 class="font-bold text-xs md:text-sm text-center mb-4 line-clamp-2">
+      {{ product.product.name }} {{ toLowercaseFirstLetterOfEachWord(product.label) }}
+    </h3>
+
+    <!-- Image du produit -->
+    <div class="ml-5">
+      <img
+        :src="product.image_url"
+        :alt="`Image de ${product.product.name}`"
+        class="object-cover w-[60px] max-h-[186px]"
+      />
     </div>
+
+    <!-- Informations prix et variante -->
+    <div class="space-y-0 absolute right-[10px] top-1/2 text-xs">
+      <div class="flex justify-end">
+        <p class="text-gray-500 font-medium">{{ product.size }}</p>
+      </div>
+      <div class="text-left">
+        <p class="font-bold text-gray-800">{{ formatPrice(product.unit_price) }}</p>
+      </div>
+    </div>
+
+    <!-- out of stock banner -->
+    <section v-if="!product.status" class="pointer-events-noner absolute inset-0">
+      <!-- Fond semi-transparent -->
+      <div class="absolute inset-0 bg-gray-400/50"></div>
+
+      <!-- Bandeau diagonal -->
+      <div
+        class="absolute top-10 -right-24 w-80 bg-red-600 text-white text-center py-3 font-bold text-sm tracking-wider transform rotate-45 shadow-lg"
+      >
+        RUPTURE DE STOCK
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { CasierProduct, Product, ProductResponse } from '@/services/locker-products/locker-products-type';
-import { formatPrice, toLowercaseFirstLetterOfEachWord } from '@/shared/shared';
-import { Minus, Plus } from 'lucide-vue-next';
-import { computed, ref, type PropType } from 'vue';
+import type {
+  CasierProduct,
+  Product,
+  ProductResponse,
+} from '@/services/locker-products/locker-products-type'
+import { formatPrice, toLowercaseFirstLetterOfEachWord } from '@/shared/shared'
+import { Minus, Plus } from 'lucide-vue-next'
+import { computed, ref, type PropType } from 'vue'
 
 // Définition des props
 // interface Props {
@@ -62,42 +96,41 @@ import { computed, ref, type PropType } from 'vue';
 // }
 
 const { product, casierProductsData } = defineProps({
-    product: {
-        type: Object as PropType<ProductResponse>,
-        required: true
-    },
-    casierProductsData: {
-        type: Object as PropType<CasierProduct>,
-        required: true
-    }
-});
+  product: {
+    type: Object as PropType<ProductResponse>,
+    required: true,
+  },
+  casierProductsData: {
+    type: Object as PropType<CasierProduct>,
+    required: true,
+  },
+})
 
 // const imageUrl = computed(() => new URL(`../../../assets/allococa/products/${product.image}`, import.meta.url).href);
 
-
 // Déclaration de l'émetteur
 const emit = defineEmits<{
-    (e: 'update:quantity', data: { id: string, newQuantity: number }): void;
-}>();
+  (e: 'update:quantity', data: { id: string; newQuantity: number }): void
+}>()
 
 // Méthodes pour incrémenter et décrémenter
 const increment = () => {
-    // quantity.value++;
-    emit('update:quantity', {
-        id: product.id,
-        newQuantity: product.quantity + 1
-    });
-};
+  // quantity.value++;
+  emit('update:quantity', {
+    id: product.id,
+    newQuantity: product.quantity + 1,
+  })
+}
 
 const decrement = () => {
-    if (product.quantity > 0) {
-        // quantity.value--;
-        emit('update:quantity', {
-            id: product.id,
-            newQuantity: product.quantity - 1
-        });
-    }
-};
+  if (product.quantity > 0) {
+    // quantity.value--;
+    emit('update:quantity', {
+      id: product.id,
+      newQuantity: product.quantity - 1,
+    })
+  }
+}
 </script>
 
 <style scoped>
